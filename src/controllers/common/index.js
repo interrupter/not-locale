22e2957a3116fdff628c50b6030123b2e452b50c
interrupter <alex.repin@mail.ru>
2021-06-05 05:52:23 +0300

    added: save/restore user preffered language

--- src/controllers/common/index.js
+++ src/controllers/common/index.js
@@ -6,6 +6,30 @@ const manifest = {
     locale:{
       default: 'ru'
     }
+  },
+  menu:{
+    top:{
+      sections:[
+				{
+					id: 'locale',
+          title: '',
+					icon: {
+						font: 'language',
+						size: 'medium',
+						svg: '',
+						src: '',
+					},
+					tag: {
+						padding: 	'small',
+						bold: 		true,
+						color: 		'success',
+						title: 		''
+					},
+					showOnTouch: true,
+					place: 'end'
+				}
+			]
+    }
   }
 };
 
--- src/controllers/common/nsLocale.js
+++ src/controllers/common/nsLocale.js
@@ -3,11 +3,14 @@
 *
 **/
 
-import {notCommon, notLocale} from 'not-bulma';
+const SECTION_ID = 'locale';
+
+import {notCommon, notLocale, TopMenu} from 'not-bulma';
 
 class nsLocale{
   constructor(app){
     this.app = app;
+    this.locales = [];
     this.app.on('wsClient:main:ready', this.update.bind(this));
     notLocale.on('change', ()=>{
       this.app.emit('locale');
@@ -27,20 +30,70 @@ class nsLocale{
   */
   async update(){
     try{
+      await this.updateAvailable();
       let res = await this.interface({locale: this.getCurrentLocale()}).$get({});
       if(res.status === 'ok' && res.result){
         notLocale.set(res.result);
       }
     }catch(e){
-      notCommon.report(e);
+      notCommon.error(e);
     }
+  }
+
+  async updateAvailable(){
+    try{
+      let res = await this.interface({}).$available({});
+      if(res.status === 'ok' && res.result){
+        this.setAvailable(res.result);
+      }
+    }catch(e){
+      notCommon.error(e);
+    }
+  }
+
+  updateUI(list){
+    let menuItems = this.createMenuItems(list);
+    TopMenu.updateSectionItems(SECTION_ID, () => {
+      return menuItems;
+    });
+    setTimeout(()=>{
+      this.app.emit(`tag-${SECTION_ID}:update`, {
+        title: this.getCurrentLocale()
+      });
+    }, 1000);
+  }
+
+  createMenuItems(list) {
+    let items = list.map(this.createMenuItem.bind(this));
+    return items;
+  }
+
+  createMenuItem(item) {
+    return {
+      id: `${SECTION_ID}.${item}`,
+      section: SECTION_ID,
+      title: item,
+      classes: ' is-clickable ',
+      action: this.changeLocale.bind(this, item)
+    };
+  }
+
+  changeLocale(locale){
+    this.saveLocaleToStore(locale);
+    this.update();
   }
 
   /**
   * @returns {string}   code of current locale
   **/
   getCurrentLocale(){
-    return this.app.getWorking('locale', this.app.getOptions('modules.locale.default', 'ru'));
+    let stored = this.restoreLocaleFromStore();
+    if (stored){
+      if(this.locales.includes(stored)){
+        return stored;
+      }
+    }
+    return this.selectBest();
   }
 
   /**
@@ -48,6 +101,48 @@ class nsLocale{
   **/
   getAvailable(){
     return this.interface().$available({});
+  }
+
+
+  setAvailable(list){
+    this.locales = list;
+    this.updateUI(list);
+  }
+
+  restoreLocaleFromStore(){
+    if (window.localStorage) {
+    	try {
+    	  return window.localStorage.getItem('locale');
+      } catch (e) {
+        this.app.error(e);
+        return false;
+      }
+    }
+    return false;
+  }
+
+  saveLocaleToStore(locale){
+    if (window.localStorage) {
+    	try {
+    	  return window.localStorage.setItem('locale', locale);
+      } catch (e) {
+        this.app.error(e);
+        return false;
+      }
+    }
+    return false;
+  }
+
+  selectBest(){
+    if(navigator.languages){
+      let locale = navigator.languages.find((itm) => {
+        return this.locales.includes(itm);
+      });
+      if(locale){
+        return locale;
+      }
+    }
+    return this.app.getWorking('locale', this.app.getOptions('modules.locale.default', 'ru'));
   }
 
 }
